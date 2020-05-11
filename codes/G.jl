e5d3f6f291e757bd8bd829501cb2125ef8e6c485
Minah Yang <lucia.yang@colorado.edu>
2020-05-11 09:52:30 -0600

    mpBQR2

---
+++ codes/G.jl
@@ -0,0 +1,36 @@
+#mpBQR3, change condition number
+include("blockHQR.jl")
+include("mpblockHQR.jl")
+include("genmat.jl")
+using DelimitedFiles, Printf
+
+name = "G"
+l = Float16; h=Float32; d=Float64;
+m = 1024; n = 1024;
+trials = 10;
+cs = exp10.(range(0, stop=2, length=9));
+berr = zeros(d, length(cs), trials);
+ferr = zeros(d, length(cs), trials);
+r = 128;
+writedlm("../txtfiles/"*name*"b.txt", ["mpBQR: backward error"])
+writedlm("../txtfiles/"*name*"f.txt", ["mpBQR: forward error"])
+for t = 1 : trials
+	@printf("\n%d: ",t)
+	for (i,c) in enumerate(cs)
+       @printf("%d\t",i)
+        A = genmat(m,n,c,l);
+        Ad = Matrix{d}(A);
+
+        # Block mixed precision
+        Q, R = bhh_QR(A, r);
+        berr[i,t] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad,2)/norm(Ad)
+        ferr[i,t] = norm(Matrix{d}(Q')*Matrix{d}(Q)-I,2)
+    end
+
+    open("../txtfiles/"*name*"b.txt", "a") do io
+        writedlm(io, [berr[:, t]'])
+    end
+    open("../txtfiles/"*name*"f.txt", "a") do io
+        writedlm(io, [ferr[:, t]'])
+    end
+end
---
+++ codes/I.jl
@@ -0,0 +1,39 @@
+#mpBQR3, change condition number
+include("blockHQR.jl")
+include("mpblockHQR.jl")
+include("genmat.jl")
+using DelimitedFiles, Printf
+
+name = "I"
+l = Float16; h=Float32; d=Float64;
+m = 1024; n = 1024;
+trials = 10;
+c = 10.
+rs = 2 .^(5:9);
+
+
+berr = zeros(d, length(rs), trials);
+ferr = zeros(d, length(rs), trials);
+
+writedlm("../txtfiles/"*name*"b.txt", ["mpBQR: backward error"])
+writedlm("../txtfiles/"*name*"f.txt", ["mpBQR: forward error"])
+for t = 1 : trials
+	@printf("\n%d: ",t)
+	for (i,r) in enumerate(rs)
+       @printf("%d\t",i)
+        A = genmat(m,n,c,l);
+        Ad = Matrix{d}(A);
+
+        # Block mixed precision
+        Q, R = mpbhh_QR(A, r);
+        berr[i,t] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad,2)/norm(Ad)
+        ferr[i,t] = norm(Matrix{d}(Q')*Matrix{d}(Q)-I,2)
+    end
+
+    open("../txtfiles/"*name*"b.txt", "a") do io
+        writedlm(io, [berr[:, t]'])
+    end
+    open("../txtfiles/"*name*"f.txt", "a") do io
+        writedlm(io, [ferr[:, t]'])
+    end
+end
