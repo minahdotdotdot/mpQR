c4bee67f6c640bf4b83d615314cab67431e8ed6a
Minah Yang <lucia.yang@colorado.edu>
2020-05-14 13:11:51 -0600

    working Y

--- codes/Y.jl
+++ codes/Y.jl
@@ -17,23 +17,26 @@ L = 2;
 writedlm("../txtfiles/"*name*"b.txt", ["Condition backward"])
 writedlm("../txtfiles/"*name*"f.txt", ["Condition orthogonal"])
 for t = 1 : trials
-	@printf("\n%d: ",t)
+	@printf("\nTrial%d: ",t)
 	for (i,n) in enumerate(ns)
-       @printf("%d\t",i)
+       @printf("n=%d, ",n)
         A = genmat(m,n,c,l);
         Ah = Matrix{h}(A);
         Ad = Matrix{d}(A);
+       @printf("%d\t",1)
 
         # Block high precision
         Q, R = bhh_QR(Ah, r);
         berr[i,t,1] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,1] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
+       @printf("%d\t",2)
         # mpBQR2
         Q, R = bhh_QR(A, r);
         berr[i,t,2] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,2] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
+       @printf("%d\t",3)
         # mpBQR3
         Q, R = mpbhh_QR(A, r);
         berr[i,t,3] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
@@ -49,25 +52,30 @@ for t = 1 : trials
           L=2
         end
         # TSQR high precision
+       @printf("%d\t",4)
         Q, R = par_TSQR(Ah, L);
         berr[i,t,4] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,4] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
         # mpTSQR2
+       @printf("%d\t",5)
         Q, R = par_TSQR(A, L);
         berr[i,t,5] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,5] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
         # mpTSQR3
+       @printf("%d\t",6)
         Q, R = mpTSQR(A, L);
         berr[i,t,6] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,6] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
+       @printf("%d\t",7)
         # hhQR in high precision
         Q, R = hh_QR(Ah);
         berr[i,t,7] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
         oerr[i,t,7] = opnorm(Matrix{d}(Q')*Matrix{d}(Q)-I)
 
+       @printf("%d\t",8)
         # hhQr in low precision
         Q, R = hh_QR(A);
         berr[i,t,8] = norm(Matrix{d}(Q)*Matrix{d}(R)-Ad)
