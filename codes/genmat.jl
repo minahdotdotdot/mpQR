86697db55364a8ae6c10f02841a96b02489c1500
Minah Yang <lucia.yang@colorado.edu>
2020-05-17 10:41:18 -0600

    save test matrices

--- codes/genmat.jl
+++ codes/genmat.jl
@@ -5,7 +5,7 @@ function genmat(m::Int,n::Int,κ::T,dt::DataType) where T<:AbstractFloat
 	#α = (κ-1)/n;
 	FL = qr(randn(h,m,n));
 	FR = qr(randn(h,n,n));
-	D = Diagonal(exp10.(-κ/(n-1)*(0:n-1)));
+	D = Diagonal(exp10.(-log10(κ)/(n-1)*(0:n-1)));
 	#A = Matrix(F.Q)*(α*ones(d,n,n)+Matrix{d}(I, n,n))
 	if dt == h
             return FL.Q*Matrix((FR.Q*D')')
---
+++ codes/savetestmats.jl
@@ -0,0 +1,20 @@
+include("genmat.jl")
+using MAT
+ms = exp10.(range(3, stop=log10(45000), length=14));
+ns = floor.(Int,ms/4);
+ms = ceil.(Int, ms);
+κs = 1e3*ones(length(ms));
+dt = l;
+name="W3"
+
+
+
+function savetestmats!(ms::Vector{Int}, ns::Vector{Int}, κs::Vector{AbstractFloats},
+	dt::DataType, name::String)
+	for i = 1 : length(ms)
+		varname=name*string(i,pad=2)
+		file = matopen("../"*varname*".mat", "w")
+		write(file, varname, genmat(ms[i],ns[i],κs[i],dt))
+		close(file)
+	end
+end
