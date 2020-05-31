7c257ba36f36664d2ebdf804b88298400aa2116c
Minah Yang <lucia.yang@colorado.edu>
2020-05-31 12:47:09 -0600

    small size, more trials

--- codes/W.jl
+++ codes/W.jl
@@ -5,44 +5,51 @@ include("mpTSQR.jl")
 include("TSQR.jl")
 
 using DelimitedFiles
-name = "mpBQR2"
+name = "sBQR3"
 l = Float16; h=Float32; d=Float64;
 #c=1000.;
 #m=4096;n=2048;
 #rs = 2 .^(6:10); 
 #berr = zeros(d, length(rs), trials,3);
 #oerr = zeros(d, length(rs), trials,3);
-m=2^13
-n=m
-rs = 2 .^ (12:-1:2)
-trials = 1;
+#m=2^13
+#n=m
+#rs = 2 .^ (12:-1:2)
+m=2^11;
+n=2^9;
+rs = 2 .^(1:9);
+trials = 10;
 writedlm("../txtfiles/"*name*"b.txt", ["Condition backward"])
 writedlm("../txtfiles/"*name*"f.txt", ["Condition orthogonal"])
 for t = 1 : trials
+    Ah = genmat(m,n,1000.0,h);
+    A = Matrix{l}(Ah);
+    Ah = Matrix{h}(Ah)
 	@printf("\n%d: ",t)
 	for (i,r) in enumerate(rs)
         @printf("%d\t",i)
         #= open("../txtfiles/"*name*"b.txt", "a") do io
             writedlm(io,['\n'])
         end =#
-        Ah = randn(h, m, n); Ah=Ah/norm(Ah)
-        A = Matrix{l}(Ah);#genmat(m,n,c,l);
-        open("../txtfiles/"*name*"b.txt", "a") do io
+        #Ah = randn(h, m, n); Ah=Ah/norm(Ah)
+        #A = Matrix{l}(Ah);#genmat(m,n,c,l);
+        #=open("../txtfiles/"*name*"b.txt", "a") do io
             writedlm(io,[string(cond(Ah))*"\n"])
-        end
+        end=#
         
-        #=
+        
         # Block high precision
         Q, R = bhh_QR(Ah, r);
         b1 = norm(Matrix{h}(Q)*Matrix{h}(R)-Ah)
         f1 = opnorm(Matrix{h}(Q')*Matrix{h}(Q)-I)
+        #=
         open("../txtfiles/"*name*"b.txt", "a") do io
             writedlm(io,[b1])
         end
         open("../txtfiles/"*name*"f.txt", "a") do io
             writedlm(io,[f1])
         end
-        =#
+        
         # mpBQR2
         Q, R = bhh_QR(A, r); Qh=Matrix{h}(Q);
         b2 = norm(Qh*Matrix{h}(R)-Ah)
@@ -53,19 +60,19 @@ for t = 1 : trials
         open("../txtfiles/"*name*"f.txt", "a") do io
             writedlm(io,[f2])
         end
-        #=
+        =#
 print("mp3\t")
         # mpBQR3
         Q, R = mpbhh_QR(A, r);
         b3 = norm(Matrix{h}(Q)*Matrix{h}(R)-Ah)
         f3 = opnorm(Matrix{h}(Q')*Matrix{h}(Q)-I)
         open("../txtfiles/"*name*"b.txt", "a") do io
-            writedlm(io,[b3])
+            writedlm(io,[b1 b3])
         end
         open("../txtfiles/"*name*"f.txt", "a") do io
-            writedlm(io,[f3])
+            writedlm(io,[f1 f3])
         end
-        =#
+        
     end
     #=open("../txtfiles/"*name*"b.txt", "a") do io
         writedlm(io, [berr[:,t,1]'])
